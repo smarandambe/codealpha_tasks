eaa50485a9e2041c6ff0a114f8d77c4a5009b9cc
smarandambe <153132164+smarandambe@users.noreply.github.com>
2026-01-29 13:20:24 +0530

    Update serializers.py

--- project_management_tool/backend/boards/serializers.py
+++ project_management_tool/backend/boards/serializers.py
@@ -6,7 +6,7 @@ from django.utils.module_loading import import_string
 from projects.models import Project
 from rest_framework import serializers
 from rest_framework.fields import Field
-from users.models import User
+from users.models import User 
 from users.serializers import UserSerializer
 
 from .models import Attachment, Board, Comment, Item, Label, List, Notification
