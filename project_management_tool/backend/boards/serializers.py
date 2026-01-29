0674e73c05cb02acedd3d0e3b3daf30047409f0c
smarandambe <153132164+smarandambe@users.noreply.github.com>
2026-01-29 13:20:46 +0530

    Update serializers.py

--- project_management_tool/backend/boards/serializers.py
+++ project_management_tool/backend/boards/serializers.py
@@ -4,7 +4,7 @@ from django.urls import Resolver404
 from django.urls.base import resolve, reverse
 from django.utils.module_loading import import_string
 from projects.models import Project
-from rest_framework import serializers
+from rest_framework import serializers 
 from rest_framework.fields import Field
 from users.models import User 
 from users.serializers import UserSerializer
